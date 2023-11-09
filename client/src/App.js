0bca6068a5f2114c170779c7530d5a441460fbf0
adarsharyan002 <adarsharyanmuz@gmail.com>
2023-11-09 14:39:12 +0530

    fix bugs in login signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Routes,Route } from 'react-router-dom'
 import LobbyScreen from './screens/LobbyScreen';
 import Room from './screens/Room';
+import LoginSignup from './components/LoginSignup';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
        <Routes>
         <Route path = '/' element={<LobbyScreen/>}/>
         <Route path = '/room/:id' element={<Room/>}/>
+        <Route path = '/login' element={<LoginSignup/>}/>
 
        </Routes>
     </div>
