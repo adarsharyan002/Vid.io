af098055a5d7f8f625aaf424d6f2b8c709d8ec82
adarsharyan002 <adarsharyanmuz@gmail.com>
2023-11-11 22:37:27 +0530

    database connected

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express();
+const { socketHandler } = require('./socket/socketLogic');
 
 const server = require("http").createServer(app);
 const cors = require("cors");
 
-const io = require("socket.io")(server, {
+
+//connect db
+const connectDB = require('./db/connect')
+
+
+//initializing socket
+ const io = require("socket.io")(server, {
 	cors: {
 		origin: "*",
 		methods: [ "GET", "POST" ]
@@ -12,9 +21,13 @@ const io = require("socket.io")(server, {
 });
 
 
+
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+
+//routes
 
 const port = process.env.PORT || 4000;
 
@@ -22,42 +35,26 @@ app.get('/',(req,res)=>{
     res.send('Server is running')
 })
 
-//socket
-
-//mapping
-const mapIdToEmail = new Map();
-const mapEmailToId = new Map();
-
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	socket.on("room:join", (data) => {
-	   const {email,room} =data;
-	   mapEmailToId.set(email,socket.id);
-	   mapIdToEmail.set(socket.id,email);
-	   io.to(room).emit("user:joined",{email,id:socket.id})
-	   socket.join(room);
-	   io.to(socket.id).emit("room:join",data);
-	
-
-	});
-
-	socket.on("user:call", ({ to,offer }) => {
-		io.to(to).emit("incoming:call", { from:socket.id,offer });
-	});
-
-	socket.on("call:accepted", ({to,ans}) => {
-		io.to(to).emit("call:accepted", { from:socket.id,ans });
-	});
-	socket.on("peer:nego:needed", ({to,offer}) => {
-		io.to(to).emit("peer:nego:needed", { from:socket.id,offer });
-	});
-	socket.on("peer:nego:done", ({to,ans}) => {
-		io.to(to).emit("peer:nego:final", { from:socket.id,ans });
-	});
-});
 
 
-server.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+const connectSocket = (socket)=>{
+	socketHandler(io,socket)
+}
+
+io.on('connection',connectSocket);
+
+
+const start = async () => {
+	try {
+	  await connectDB(process.env.MONGO_URI)
+	  server.listen(port, () =>
+		console.log(`Server is listening on port ${port}...`)
+	  );
+	} catch (error) {
+	  console.log(error);
+	}
+  };
+  
+  start();
+  
+
