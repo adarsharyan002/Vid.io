d4ec5375fea26226990470775c426291cfa116d5
YashaHirekhan <yashahirekhan@gmail.com>
2023-12-19 12:59:23 +0530

    .

--- client/src/components/LoginSignup.js
+++ client/src/components/LoginSignup.js
@@ -9,83 +9,17 @@ import { signUpUser } from '../redux/features/auth/authSlice';
 const LoginSignup = () => {
 
   const dispatch = useDispatch();
-  const loginSignupState = useSelector((state)=> state.loginSignup)
-  const [email, setEmail] = useState(loginSignupState.email);
-  const [password, setPassword] = useState(loginSignupState.email);
   
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    dispatch(setEmail(e.target.value));
-  };
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [action, setAction] = useState("Create Your Account");  
 
-  
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    dispatch(setPassword(e.target.value));
-  };
 
-  const handleSigninSubmit = (e) => {
-   
-  };
-
-  
-
-//   return (
-//     <div className='whole-page'>
-//         <div className='container'>
-//             <div className="header">Video Calling App</div>
-//             <div className='login-signup'>{action}</div>
-//             <div className='text'> 
-//                 <div>Record vedios to share your</div>
-//                 <div>feedback and ideas.</div> 
-//             </div>
-//             <div className='gap'>
-//             <div className='button-4'>
-//                 <div>Continue with Google</div>
-//             </div>
-            
-//             </div>
-
-//             <div>or</div>
-
-//             <div className='inputs'>
-//                 <input className='input' type="email" placeholder='E-mail'/>
-//             </div>
-//             <div className='inputs'>
-//                 <input className='input' type="passwoed" placeholder='Password'/>
-//             </div>
-//             <div className='forgot-password'>
-//             {action==="Sign in"?<span>Forgot password?</span>:<div></div>}
-//             </div>
-//             <div className='submit-container'>
-//                 {action==="Create Your Account"?<div className="button-3" > Sign Up </div>:<div></div>}
-//                 {action==="Sign in"?<div className="button-3" >Login</div>:<div></div>}
-//             </div>
-//             <div className='already-have-an-account'>
-//                 {action==="Create Your Account"?<div className='text'>Already have an account? </div>:<div></div>}
-//                 {action==="Create Your Account"?<span className='text' onclick onClick={()=>{dispatch(setAction("Sign in"))}}>Sign in</span>:<div></div>}
-//                 {action==="Sign in"?<span className="text" onclick onClick={()=>{dispatch(setAction("Create Your Account"))}}>Signup</span>:<div></div>}
-//             </div>
-      
-//         </div>
-//         <div className='vedio-calling-image'>
-//             <img src={vedioCalling} alt='Vedio calling image'/>
-            
-//         </div>
-
-//     </div>
-    
-    
-//   )
-// }
-
-// export default LoginSignup
-
-return (
+  return (
     <div className='whole-page'>
         <div className='container'>
             <div className="header">Video Calling App</div>
-            <div className='login-signup'>{loginSignupState.action}</div>
+            <div className='login-signup'>{action}</div>
             <div className='text'> 
                 <div>Record vedios to share your</div>
                 <div>feedback and ideas.</div> 
@@ -100,22 +34,22 @@ return (
             <div>or</div>
 
             <div className='inputs'>
-                <input className='input' type="email" placeholder='E-mail' onChange={handleEmailChange}/>
+                <input className='input' type="email" placeholder='E-mail'/>
             </div>
             <div className='inputs'>
-                <input className='input' type="passwoed" placeholder='Password' onChange={handlePasswordChange}/>
+                <input className='input' type="passwoed" placeholder='Password'/>
             </div>
             <div className='forgot-password'>
-            {loginSignupState.action==="Sign in"?<span>Forgot password?</span>:<div></div>}
+            {action==="Sign in"?<span>Forgot password?</span>:<div></div>}
             </div>
             <div className='submit-container'>
-                {loginSignupState.action==="Create Your Account"?<div className="button-3" onClick={handleSigninSubmit}> Sign Up </div>:<div></div>}
-                {loginSignupState.action==="Sign in"?<div className="button-3" onClick={handleSigninSubmit} >Login</div>:<div></div>}
+                {action==="Create Your Account"?<div className="button-3" > Sign Up </div>:<div></div>}
+                {action==="Sign in"?<div className="button-3" >Login</div>:<div></div>}
             </div>
             <div className='already-have-an-account'>
-                {loginSignupState.action==="Create Your Account"?<div className='text'>Already have an account? </div>:<div></div>}
-                {loginSignupState.action==="Create Your Account"?<span className='text' onClick={handleSigninSubmit}>Sign in</span>:<div></div>}
-                {loginSignupState.action==="Sign in"?<span className="text" onClick={handleSigninSubmit}>Signup</span>:<div></div>}
+                {action==="Create Your Account"?<div className='text'>Already have an account? </div>:<div></div>}
+                {action==="Create Your Account"?<span className='text' onclick onClick={()=>{dispatch(setAction("Sign in"))}}>Sign in</span>:<div></div>}
+                {action==="Sign in"?<span className="text" onclick onClick={()=>{dispatch(setAction("Create Your Account"))}}>Signup</span>:<div></div>}
             </div>
       
         </div>
